2c052e9e319198a0e8358ff58c6123967dfd6e26
Fabio Rauber <fabiorauber@gmail.com>
2016-03-31 12:16:49 -0300

    First working version

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # hiera-openstack backend
 
-This module allows hiera to look up entries in Openstack Compute (Nova) Metadata. 
+This module allows hiera to look up entries in Openstack Compute (Nova) Metadata.  
 
 # Installation
 
@@ -15,7 +15,17 @@ gem install openstack
 # Use
 
 ## Configuration example
+
+The following configuration will allow Hiera to get all properties of the instance with the name == "%{::hostname}". 
+
 <pre>
+# /etc/puppet/hiera.yaml
+---
+:backends:
+  - openstack
+
+:hierarchy:
+  - "%{::hostname}"
 
 :openstack:
   :auth_url: https://your.openstack.cloud.domain:5000/v2.0
@@ -25,7 +35,25 @@ gem install openstack
 
 </pre>
 
-## Puppet example
+## Hiera keys
+
+The following properties are defined, in addition of whatever the user defines inside the instance metadata:
+
+- id
+- name
+- status
+- progress
+- accessipv4
+- accessipv6
+- addresses
+- hostId
+- image
+- flavor
+- metadata
+- adminPass
+- key_name
+- created
+- security_groups
 
 
 # Details
--- lib/hiera/backend/openstack_backend.rb
+++ lib/hiera/backend/openstack_backend.rb
@@ -18,6 +18,13 @@ class Hiera
           :service_type    => "compute"})
       end
 
+      def hashit(object)
+        properties = Hash.new
+        object.instance_variables.each {|x| properties[x[1..-1]] = object.instance_variable_get(x) }
+        return properties
+      end
+
+
       # Helper for parsing config. Does not Hiera provide one?
       def get_config_value(label, default)
         if Config.include?(:openstack) && Config[:openstack].include?(label)
@@ -29,19 +36,49 @@ class Hiera
 
       def lookup(key, scope, order_override, resolution_type)
         conf = Config[:openstack] 
-
         answer = []
-       
+
         Hiera.debug("Looking up #{key} in openstack metadata backend")
 
-        #Backend.datasources(scope, order_override) do |source|
-        #  Hiera.debug("Looking for data source #{source}")
-        #  begin
+        Backend.datasources(scope, order_override) do |source|
+          Hiera.debug("Looking for data source #{source}")
+          @connection.servers.each do |server| 
+            Hiera.debug("Server: #{server[:name]} with id #{server[:id]}")
+            if server[:name] == source
+              if @connection.server(server[:id]).respond_to?(key)
+                property = @connection.server(server[:id]).send(key)
+                case key
+                when 'addresses'
+                  property.each do |addr|
+                    answer.push hashit(addr)
+                  end
+                when 'flavor'
+                  oflavor = @connection.flavor(property['id'])
+                  answer.push hashit(oflavor)
+                when 'metadata'
+                  meta = []
+                  property.each_pair do |k , v|
+                    meta.push [ k , v ]
+                  end
+                  answer.push Hash[meta]
+                else
+                  answer.push property
+                end
+              end
+              @connection.server(server[:id]).metadata.each_pair do |k , v|
+                Hiera.debug("Found metadata with key #{k}.")
+                if k == key
+                  answer.push v 
+                end
+              end
+            end
+          end
+        end #end datasources
 
-        # end #end datasources
-      rescue Exception => e
-        Hiera.debug("Exception: #{e}")
-      end
+        return answer
+        rescue Exception => e
+          Hiera.debug("Exception: #{e}")
+        end
     end
   end
 end
